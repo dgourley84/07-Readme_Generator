634fe282687224625c668ef38d463df9ae9add5f
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-10-17 16:53:40 +1000

    Update default value

--- Develop/utils/questions.js
+++ Develop/utils/questions.js
@@ -4,7 +4,7 @@ const questions = [
         type: 'input',
         message: "What is your GitHub username? (No @ needed)",
         name: 'userName',
-        default: 'dallasg84',
+        default: 'dgourley84',
         validate: function (answer) {
             if (answer.length < 1) {
                 return console.log("A valid GitHub username is required.");
